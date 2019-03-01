1e35e1a84a593803b614bccdeb1d20a8a0d986ae
abcd@1234 <chenzuo@hotmail.com>
2019-03-02 02:08:17 +0800

    update

--- readMe.txt
+++ readMe.txt
@@ -1,4 +1,7 @@
 1、npm install webpack --save-dev
 2、npm install html-webpack-plugin --save-dev
 3、npm install webpack-cli --save-dev
-3、create webpack.config.js
+4、create webpack.config.js
+5、npm install --save-dev style-loader css-loader 
+
+6、command:C#> webpack -p 
--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,15 @@ module.exports = {
         //我们的是多页面项目，多页面入口配置就是这样，
         //app/src/page下可能还会有很多页面，照着这样配置就行
     },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }, ]
+    },
     output: {
         //__dirname 当前webpack.config.js的路径
-        filename: '[name].js', //打包后index.js的名字，
+        filename: '[name].[chunkHash:5].js', //打包后index.js的名字，
         // 这个[name]的意思是,配置入口entry键值对里的key值,app/dist/js/index,最后的index，
         //这里无论你src/js/index.js这个脚本如何命名，打包后都将是index.js
         //   path: path.resolve(__dirname)
