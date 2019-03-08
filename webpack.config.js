6fdf6b6aff3a1951e5d0d674fe6b843bb873fdea
abcd@1234 <chenzuo@hotmail.com>
2019-03-08 15:04:50 +0800

    修正打包文件配置路径问题

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var cleanwebpackplugin = require('clean-webpack-plugin'); //清理打包目录
 var MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     entry: {
-        './index': './app/src/js/index.js' //入口文件
+        'index': './app/src/js/index.js' //入口文件
     },
     module: {
         rules: [{
@@ -39,7 +39,8 @@ module.exports = {
         new cleanwebpackplugin(),
         new HtmlWebpackPlugin({
             filename: 'index.html',
-            template: 'app/src/index.html'
+            template: 'app/src/index.html',
+            hash: true, //为了更好的 cache，可以在文件名后加个 hash。
         }),
         new MiniCssExtractPlugin({
             filename: "css/[name].[chunkHash:5].css"
